502e5f0666d1ddb3ddb66a48aed09598aa6f9a99
paul88vera <paul88vera@gmail.com>
2022-01-12 21:44:05 -0600

    issue #1 completed

--- __tests__/random.test.js
+++
@@ -1,6 +0,0 @@
-const { TestWatcher } = require('jest');
-const checkIfEqual = require('../lib/random.js');
-
-test('checks if 0 is equal to 10', () => {
-  expect(checkIfEqual(10,10)).toBe(true);
-});
